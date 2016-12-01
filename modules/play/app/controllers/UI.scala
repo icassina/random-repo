779e67e48310e90cc27ab2ba8d850d0ab3172a45
Ilya Cassina <icassina@gmail.com>
2016-12-01 23:26:04 +0100

    removed WebSocket completely

--- modules/play/app/controllers/UI.scala
+++ modules/play/app/controllers/UI.scala
@@ -2,9 +2,6 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import akka.actor.ActorSystem
-import akka.stream.Materializer
-
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.streams._
@@ -12,13 +9,11 @@ import play.api.libs.streams._
 import icassina.lunatech._
 
 @Singleton
-class UI @Inject() (dao: DAO, daoEC: DAOExecutionContext, actorSystem: ActorSystem, m: Materializer) extends Controller {
+class UI @Inject() (dao: DAO, daoEC: DAOExecutionContext) extends Controller {
 
   private val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
 
   implicit val ec = daoEC
-  implicit val as: ActorSystem = actorSystem
-  implicit val mr: Materializer = m
 
   def index = Action.async { implicit request =>
     dao.stats.map(stats => Ok(views.html.index(stats)))
@@ -35,9 +30,5 @@ class UI @Inject() (dao: DAO, daoEC: DAOExecutionContext, actorSystem: ActorSyst
 
   def query = Action { implicit request =>
     Ok(views.html.query())
-  }
-
-  def ws = WebSocket.accept[JsValue, JsValue] { implicit request =>
-    ActorFlow.actorRef(WSActor.props(_)(dao, ec))
   }
 }
